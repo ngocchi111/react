7413470f8c2e5919eb7362a43514b6c9a29b2097
ngocchi111 <68102728+ngocchi111@users.noreply.github.com>
2021-11-03 18:24:59 +0700

    delete file error

--- src/components/classes/Orders.js
+++ src/components/classes/Orders.js
@@ -42,7 +42,7 @@ export default function Orders() {
 
   function handleClick(e){
     e.preventDefault();
-    fetch('https://apinc.herokuapp.com/classes', {
+    fetch('http://localhost:3000/classes', {
     method: "POST",
     body: JSON.stringify({
       name: item.name,
@@ -65,7 +65,7 @@ export default function Orders() {
   };
 
     useEffect(() =>{
-        fetch("https://apinc.herokuapp.com/classes")
+        fetch("http://localhost:3000/classes")
         .then( res => res.json())
         .then(
             (result)=>{
